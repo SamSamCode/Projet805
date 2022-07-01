ebb4ae3b108458b7b14e2df818efabdd178ae81f
kevin <kevin.charlier@etudiant.univ-reims.fr>
2022-07-02 00:12:57 +0200

    Projet Tracker

---
+++ Tracker/src/main/java/Activity.java
@@ -0,0 +1,17 @@
+public class Activity {
+	private String name;
+
+	public Activity() {
+		name = "";
+	}
+
+	// Getter
+	public String getName() {
+		return name;
+	}
+
+	// Setter
+	public void setName(String newName) {
+		this.name = newName;
+	}
+}
